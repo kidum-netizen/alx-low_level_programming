bbace7a7cd7efae85dd5084a886673c9ec75c31d
kidum-netizen <frankkibet96@gmail.com>
2023-07-28 11:53:45 -0700

    singlylinked

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,12 +9,12 @@
  */
 size_t list_len(const list_t *h)
 {
- size_t q = 0;
+size_t q = 0;
 
- while (h)
- {
- q++;
- h = h->next;
- }
- return (q);
+while (h)
+{
+q++;
+h = h->next;
+}
+return (q);
 }
